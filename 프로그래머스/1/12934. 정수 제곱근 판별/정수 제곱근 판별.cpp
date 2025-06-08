3f02d0ab3a1350ab89d8cf2fd6a16d7818365961
DaegeunChoe <asdf12gg@gmail.com>
2025-06-08 20:47:57 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.01 ms, Memory: 3.64 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+long long solution(long long n) {
+    long long root = sqrt(n); 
+    if (root * root == n)
+    {
+        return (root + 1)*(root + 1);
+    }
+    else 
+    {
+        return -1;
+    }
+}
