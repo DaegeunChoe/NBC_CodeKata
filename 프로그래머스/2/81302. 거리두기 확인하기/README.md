62876f343ece5e48f30eaf2a3301b91d78e52f3b
DaegeunChoe <asdf12gg@gmail.com>
2025-06-20 10:33:24 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/README.md
+++ 프로그래머스/2/81302. 거리두기 확인하기/README.md
@@ -1,6 +1,6 @@
 # [level 2] 거리두기 확인하기 - 81302 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81302) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81302?language=cpp) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 20일 10:32:39
+2025년 06월 20일 10:33:22
 
 ### 문제 설명
 
--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.cpp
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.cpp
@@ -1,18 +1,19 @@
 #include <string>
 #include <vector>
+#include <cctype>
 #define ROW 5
 #define COL 5
 using namespace std;
 
-bool isValid(int r, int c, vector<string>& place) {
+bool isValid(int r, int c, char place[][COL]) {
     // 맨하탄 거리가 1인 지점은 총 4군데
     if (r-1 >= 0 && place[r-1][c] == 'P') {
         return false;
     }
-    if (c-1 >= 0 && place[r][c-1] == 'P') {
+    if (r+1 < ROW && place[r+1][c] == 'P') {
         return false;
     }
-    if (r+1 < ROW && place[r+1][c] == 'P') {
+    if (c-1 >= 0 && place[r][c-1] == 'P') {
         return false;
     }
     if (c+1 < COL && place[r][c+1] == 'P') {
@@ -22,28 +23,42 @@ bool isValid(int r, int c, vector<string>& place) {
     // 맨하탄 거리가 2인 지점은 총 8군데
     // 직선 방향
     if (r-2 >= 0 && place[r-2][c] == 'P') {
-        if (place[r-1][c] != 'X') {
-            return false;
+        if (place[r-1][c] == 'X') {
+            return true;
         }
-    }
-    if (c-2 >= 0 && place[r][c-2] == 'P') {
-        if (place[r][c-1] != 'X') {
-            return false;
-        }
-    }
-    if (c+2 < COL && place[r][c+2] == 'P') {
-        if (place[r][c+1] != 'X') {
+        else {
             return false;
         }
     }
     if (r+2 < ROW && place[r+2][c] == 'P') {
-        if (place[r+1][c] != 'X') {
+        if (place[r+1][c] == 'X') {
+            return true;
+        }
+        else {
             return false;
         }
     }
+    if (c-2 >= 0 && place[r][c-2] == 'P') {
+        if (place[r][c-1] == 'X') {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    if (c+2 < COL && place[r][c+2] == 'P') {
+        if (place[r][c+1] == 'X') {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    
     // 대각선 방향
     if (r-1 >= 0 && c-1 >= 0 && place[r-1][c-1] == 'P') {
         if (place[r-1][c] == 'X' && place[r][c-1] == 'X') {
+            return true;
         }
         else {
             return false;
@@ -51,6 +66,7 @@ bool isValid(int r, int c, vector<string>& place) {
     }
     if (r-1 >= 0 && c+1 < COL && place[r-1][c+1] == 'P') {
         if (place[r-1][c] == 'X' && place[r][c+1] == 'X') {
+            return true;
         }
         else {
             return false;
@@ -58,6 +74,7 @@ bool isValid(int r, int c, vector<string>& place) {
     }
     if (r+1 < ROW && c-1 >= 0 && place[r+1][c-1] == 'P') {
         if (place[r+1][c] == 'X' && place[r][c-1] == 'X') {
+            return true;
         }
         else {
             return false;
@@ -65,6 +82,7 @@ bool isValid(int r, int c, vector<string>& place) {
     }
     if (r+1 < ROW && c+1 < COL && place[r+1][c+1] == 'P') {
         if (place[r+1][c] == 'X' && place[r][c+1] == 'X') {
+            return true;
         }
         else {
             return false;
@@ -74,10 +92,13 @@ bool isValid(int r, int c, vector<string>& place) {
     return true;
 }
 
-bool checker(vector<string>& place) {
+bool checker(char place[][COL]) {
     
     for (int r=0; r<ROW; r++) {
         for (int c=0; c<COL; c++) {
+            if (place[r][c] != 'P' && place[r][c] != 'X' && place[r][c] != 'O') {
+                while (true) {}
+            }
             if (place[r][c] == 'P') {
                 if (!isValid(r, c, place)) {
                     return false;
@@ -90,9 +111,14 @@ bool checker(vector<string>& place) {
 
 vector<int> solution(vector<vector<string>> places) {
     vector<int> answer;
-    
     for (auto& place : places) {
-        if (checker(place)) {
+        char p[ROW][COL];
+        for (int r=0; r<ROW; r++) {
+            for (int c=0; c<COL; c++) {
+                p[r][c] = place[r][c];
+            }
+        }
+        if (checker(p)) {
             answer.push_back(1);
         }
         else {
